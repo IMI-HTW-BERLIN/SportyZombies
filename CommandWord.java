070acf4f879a9657ea62d6446e38d81565883858
David <panagiotopulos.d@gmail.com>
2018-01-09 13:56:29 +0100

    Initial sharing of project

---
+++ CommandWord.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+/**
+ * Write a description of class CommandWord here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CommandWord
+{
+    HashMap<String, CommandWords> commands = new HashMap<>();
+
+    /**
+     * Constructor - initialise the command words.
+     */
+    CommandWord()
+    {
+        commands.put("go", CommandWords.GO);
+        commands.put("help", CommandWords.HELP);
+        commands.put("quit", CommandWords.QUIT);
+        commands.put("look", CommandWords.LOOK);
+    }
+    
+    /**
+     * Check whether a given String is a valid command word. 
+     * @return true if a given string is a valid command,
+     * false if it isn't.
+     */
+    public boolean isCommand(String cmd)
+    {
+        return commands.containsKey(cmd);
+    }
+}
---
+++ Location.java
@@ -0,0 +1,75 @@
+import java.util.HashMap;
+/**
+ * Class Location - a Location in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Location" represents one location in the scenery of the game.  It is 
+ * connected to other Locations via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the Location stores a reference
+ * to the neighboring Location, or null if there is no exit in that direction.
+ * 
+ * @author  Michael Kolling and David J. Barnes
+ * @version 2008.03.30
+ */
+public class Location 
+{
+    private HashMap<String, Location> exits;
+    private String description;
+
+    /**
+     * Create a Location described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The Location's description.
+     */
+    public Location(String description) 
+    {
+        this.description = description;
+        exits = new HashMap<>();
+    }
+
+    /**
+     * Define the exits of this Location.  Every direction either leads
+     * to another Location or is null (no exit there).
+     * @param north The north exit.
+     * @param east The east east.
+     * @param south The south exit.
+     * @param west The west exit.
+     */
+    public void addExit(String name, Location exit) 
+    {
+        exits.put(name, exit);
+    }
+
+    /**
+     * @return The description of the Location.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    /**
+     * @return The long description of the Location.
+     */
+    public String getLongDescription()
+    {
+        return "You are " + getDescription() + "\n" + getExits();
+    }
+    
+    public String getExits(){
+        String allExits = "Exits: ";
+        for(String name : exits.keySet()){
+            allExits += name + ", ";
+        }
+        allExits = allExits.replaceAll(", $","");
+        
+        return allExits;
+    }
+    
+    public Location getExit(String exit){
+        return exits.get(exit);
+    }
+}
